eb3b37e4475517b0577dfb891573f8868d2d5c78
Mykaellen Dias <msylvinalealdias@gmail.com>
2022-01-14 18:34:51 -0300

    Form layout

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,6 +1,42 @@
+import './styles.css';
+
 function Form() {
-    return(
-        <h1>Página de formulário</h1>
+
+    const movie = {
+        id: 1,
+        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+        title: "The Witcher",
+        count: 2,
+        score: 4.5
+    };
+
+    return (
+        <div className="mykamovie-form-container">
+            <img className="mykamovie-movie-card-image" src={movie.image} alt={movie.title} />
+            <div className="mykamovie-card-bottom-container">
+                <h3>{movie.title}</h3>
+                <form className="mykamovie-form">
+                    <div className="form-group mykamovie-form-group">
+                        <label htmlFor="email">Informe seu email</label>
+                        <input type="email" className="form-control" id="email" />
+                    </div>
+                    <div className="form-group mykamovie-form-group">
+                        <label htmlFor="score">Informe sua avaliação</label>
+                        <select className="form-control" id="score">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
+                    <div className="mykamovie-form-btn-container">
+                        <button type="submit" className="btn btn-primary mykamovie-btn">Salvar</button>
+                    </div>
+                </form >
+                <button className="btn btn-primary mykamovie-btn mt-3">Cancelar</button>
+            </div >
+        </div >
     );
 }
 
